58a9ed929f9031436ca99b5c11bf7f0b2ad7426b
Siarhei Voitau <sevo@Siarheis-MacBook-Pro.local>
2022-09-08 20:56:44 +0200

    add redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,40 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "sv"],
+    localeDetection: false
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "query",
+            key: "withoutLocale",
+            value: "true",
+          },
+        ],
+        permanent: false,
+        destination: "/redirected",
+      },
+      {
+        source: "/",
+        has: [
+          {
+            type: "query",
+            key: "withLocale",
+            value: "true",
+          },
+        ],
+        locale: false,
+        permanent: false,
+        destination: "/redirected",
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
